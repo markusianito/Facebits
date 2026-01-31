de67650bd67cec32c604a57b91445e65a8a37327
Marcos Rubiano <markusianito@proton.me>
2026-01-30 20:57:16 -0500

    C++11+ support, v2.0.0

--- src/Facebits.h
+++ src/Facebits.h
@@ -9,16 +9,48 @@
 	This program is licensed under MIT license. See LICENSE file.
 */
 
-#ifndef FACEBITS
-#define FACEBITS
+#pragma once
 
 #include <inttypes.h>
 
-template<typename T> T maskShift(T mask);
-template<typename T> T maskWidth(T mask);
-template<typename T> T readData(T info, T mask);
-template<typename T, typename U> T writeDataPreview(T info, T mask, U data);
-template<typename T, typename U> void writeData(T& info, T mask, U data);
 
+template<typename T> T maskShift(T mask)
+{
+	for (uint8_t i = 0; i < sizeof(T)*8; i++)
+		if (mask & (T(1) << i))
+			return i;
+	return 0;
+}
 
-#endif // FACEBITS
+template<typename T> T maskWidth(T mask)
+{
+	uint8_t w = 0;
+	while (mask)
+	{
+		w += mask & T(1);
+		mask >>= 1;
+	}
+	return w;
+}
+
+template<typename T> T readData(T info, T mask)
+{
+	uint8_t shift = maskShift(mask);
+	return (info & mask) >> shift;
+}
+
+template<typename T, typename U> void writeData(T& info, T mask, U data)
+{
+	uint8_t shift = maskShift(mask);
+	uint8_t width = maskWidth(mask);
+	T maxVal = (T(1) << width) - T(1);
+
+	T result = static_cast<T>(data) & maxVal;
+
+	info = (info & ~mask) | (result << shift);
+}
+template<typename T, typename U> T writeDataPreview(T info, T mask, U data)
+{
+	writeData(info, mask, data);
+    return info;
+}
